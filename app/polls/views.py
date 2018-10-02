189af5932bc8cfd230d1933dc2647d9626c8e659
Jeon Young Hoon <hungyb0924@gmail.com>
2018-10-02 17:31:09 +0900

    try-except 구문을 get_object_or_404함수로 대체

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, response, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
 from .models import Question
@@ -8,12 +8,15 @@ from .models import Question
 def index(request):
     # Question 클래스에 대한 QuerySet을 가져옴
     # 게시일자 속성에 대한 내림차순 순서로 최대 5개까지
+
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
+
     # 가져온 Question QuerySet을 사용 , 각 Question의 question_text 속성값들ㅇ르
     # list comprehension을 사용해 리스트로 생성
     # 생성한 리스트를 ', ' 문자열의 join 메서드의 인수로 전달, output 에 쉼표 단위로 연결된 문자열을 할당.
@@ -21,10 +24,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Qustion does not exit')
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Qustion does not exit')
+    question = get_object_or_404(Question, pk=question_id)
 
     context = {
         'question': question,
